e04ccc974d8b82fbab7c5dab34158003e0a00809
Thiago Tolentino <30497459+ThiagoTolentino@users.noreply.github.com>
2021-05-26 17:14:29 -0300

    Add files via upload

---
+++ src/main/java/account/GoogleAccount.java
@@ -0,0 +1,40 @@
+package account;
+
+public class GoogleAccount {
+    private String user;
+    private String passwd;
+    private String name;
+    private String lastName;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
